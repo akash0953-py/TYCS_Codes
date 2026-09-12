412f1671207be403bef5c95fa96b2d05e5e82581
akash0953-py <akashgowda0412@gmail.com>
2026-09-12 13:27:20 +0530

    Linear Algebra pr 10

---
+++ Linear Algebra/pr10.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+try:
+    l = int(input("Enter length: "))
+    v = np.array([int(input(f"Enter v[{i+1}]: ")) for i in range(l)])
+    u = np.array([int(input(f"Enter u[{i+1}]: ")) for i in range(l)])
+
+    a = int(input("Enter scalar a: "))
+    b = int(input("Enter scalar b: "))
+
+    linear_combination = a*u + b*v
+    average = (u + v) / 2
+
+    print("Linear Combination:", linear_combination)
+
+except Exception as e:
+    print(e)
