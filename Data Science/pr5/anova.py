4216d222a687fd937305b3c7e0ab9595f5906bcc
akash0953-py <akashgowda0412@gmail.com>
2026-09-21 19:36:41 +0530

    ANOVE PR5 added

---
+++ Data Science/pr5/anova.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy import stats
+import pandas as pd
+
+df = pd.read_csv("College.csv")
+
+ves = np.array(df['Vesasc'])
+nga = np.array(df['Nga'])
+bt = np.array(df['BT'])
+
+n =5
+k =3
+N =15
+
+ves_mean = ves.mean()
+nga_mean = nga.mean()
+bt_mean = bt.mean()
+
+total_mean = np.array([ves_mean ,nga_mean , bt_mean]).mean()
+
+a = (ves_mean - total_mean)**2
+b = (nga_mean - total_mean)**2
+c = (bt_mean - total_mean)**2
+
+total_sum_mean = n * (a+b+c)
+ssb = total_sum_mean / (k-1)
+
+ssw = (sum((ves - ves_mean) **2 ) + sum((nga - nga_mean) ** 2) + sum((bt - bt_mean) ** 2)) / (N-k)
+
+f = ssb/ ssw
+p = stats.f.sf(f,(k-1),(N-k))
