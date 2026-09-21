d87cc7fb309e9ec20b00b5e9325825d6e3c27aec
akash0953-py <akashgowda0412@gmail.com>
2026-09-21 19:43:51 +0530

    ANOVE PR5 updated

--- Data Science/pr5/anova.py
+++ Data Science/pr5/anova.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy import stats
 import pandas as pd
 
-df = pd.read_csv("College.csv")
+df = pd.read_csv("Data Science\pr5\College.csv")
 
 ves = np.array(df['Vesasc'])
 nga = np.array(df['Nga'])
@@ -29,3 +29,13 @@ ssw = (sum((ves - ves_mean) **2 ) + sum((nga - nga_mean) ** 2) + sum((bt - bt_me
 
 f = ssb/ ssw
 p = stats.f.sf(f,(k-1),(N-k))
+
+print("Variance between the group :- ",ssb)
+print("Variance within the group :- ", ssw)
+print("F statistics score :- ", f)
+print("P-value :- ", p)
+
+if p < 0.05:
+    print("Reject Null Hypothesis")
+else:
+    print("Fail to Reject Null Hypothesis")
